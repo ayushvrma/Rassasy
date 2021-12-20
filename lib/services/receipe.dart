6a8e677be59e2bc65e19307a7f3e70ed40d65a97
Ayush <ayush8402@gmail.com>
2021-12-20 23:39:05 +0530

    first commit

---
+++ lib/services/receipe.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:recipe_app/models/hits.dart';
+import 'package:http/http.dart' as http;
+
+class Recipie {
+  List<Hits> hits = [];
+
+  Future<void> getReceipe() async {
+    String url =
+        "https://api.edamam.com/search?q=banana&app_id=c272a71d&app_key=4044f53a740ab0304622a56eefc46dba";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+    jsonData["hits"].forEach((element) {
+      Hits hits = Hits(
+        recipeModel: element['recipe'],
+      );
+      // hits.recipeModel = add(Hits.fromMap(hits.recipeModel));
+    });
+  }
+}
